f284942eb8d17d27edb293fb402e4ab7bcceb732
guplem <guplem@gmail.com>
2021-11-26 19:01:30 +0100

    An app with a body

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -3,17 +3,15 @@ import "package:flutter/material.dart";
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Hello World!"),
         ),
+        body: Text("Body Text"),
       ),
     );
   }
-
 }
