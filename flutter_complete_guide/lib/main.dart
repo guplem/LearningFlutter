8e6bffc19b4ae0d7cebf046b4ec3eddab9dd45af
guplem <guplem@gmail.com>
2021-11-26 20:13:33 +0100

    Button executes a function

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -3,6 +3,11 @@ import "package:flutter/material.dart";
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  void answerQuestion(){
+    print ("Answer chosen!");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,15 +20,15 @@ class MyApp extends StatelessWidget {
             Text("Question:"),
             ElevatedButton( //Previously (deprecated) was RaisedButton
               child: Text("Answer 1"),
-              onPressed: null,
+              onPressed: answerQuestion, //Be aware, do not pass the result of the function ("answerQuestion()") but the pointer to the function ("answerQuestion")
             ),
             ElevatedButton( //Previously (deprecated) was RaisedButton
               child: Text("Answer 2"),
-              onPressed: null,
+              onPressed: answerQuestion, //Be aware, do not pass the result of the function ("answerQuestion()") but the pointer to the function ("answerQuestion")
             ),
             ElevatedButton( //Previously (deprecated) was RaisedButton
               child: Text("Answer 4"),
-              onPressed: null,
+              onPressed: answerQuestion, //Be aware, do not pass the result of the function ("answerQuestion()") but the pointer to the function ("answerQuestion")
             ),
           ],
         ),
