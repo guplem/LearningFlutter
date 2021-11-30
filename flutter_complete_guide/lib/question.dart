82ec7a2a49391efa146042fc8f7994da025621b3
guplem <guplem@gmail.com>
2021-11-30 12:47:57 +0100

    Styling

--- flutter_complete_guide/lib/question.dart
+++ flutter_complete_guide/lib/question.dart
@@ -3,10 +3,18 @@ import 'package:flutter/material.dart';
 class Question extends StatelessWidget {
   final String questionText; // With every update, flutter will create a new one
 
-  Question (this.questionText); // Constructor
+  Question(this.questionText); // Constructor
 
   @override
   Widget build(BuildContext context) {
-    return Text("Question from C widget: " + questionText);
+    return Container(
+      width: double.infinity, // as much as possible
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
