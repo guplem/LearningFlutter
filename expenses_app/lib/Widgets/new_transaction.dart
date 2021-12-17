dcd0a80961de5914a072d0054b328c0ee604a7ca
guplem <guplem@gmail.com>
2021-12-17 18:37:03 +0100

    Keyboard adaptive size

--- expenses_app/lib/Widgets/new_transaction.dart
+++ expenses_app/lib/Widgets/new_transaction.dart
@@ -21,62 +21,63 @@ class _NewTransactionState extends State<NewTransaction> {
       initialDate: DateTime.now(),
       firstDate: DateTime(2020),
       lastDate: DateTime.now(),
-
-    ).then((pickedDate){
-
-      if (pickedDate == null)
-        return;
+    ).then((pickedDate) {
+      if (pickedDate == null) return;
 
       setState(() {
         _selectedDate = pickedDate;
       });
-
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: "Title"),
-              controller: _titleController,
-              onSubmitted: (_) => _SubmitData(),
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: "Amount"),
-              controller: _amountController,
-              keyboardType: TextInputType.number,
-              onSubmitted: (_) => _SubmitData(),
-            ),
-            Container(
-              height: 70,
-              child: Row(
-                children: [
-                  Expanded(child: Text(_selectedDate == null? "No Date Chosen" : "Picked Date:" + DateFormat.yMd().format(_selectedDate!) )),
-                  TextButton(
-                    onPressed: _presentDatePicker,
-                    child: const Text(
-                      "ChooseDate",
-                    ),
-                  )
-                ],
+    return SingleChildScrollView(
+      child: Card(
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            bottom: 10 + MediaQuery.of(context).viewInsets.bottom, // Keyboard area
+            left: 10,
+            right: 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: "Title"),
+                controller: _titleController,
+                onSubmitted: (_) => _SubmitData(),
               ),
-            ),
-            ElevatedButton(onPressed: _SubmitData, child: Text("Add Transaction"))
-          ],
+              TextField(
+                decoration: InputDecoration(labelText: "Amount"),
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => _SubmitData(),
+              ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    Expanded(child: Text(_selectedDate == null ? "No Date Chosen" : "Picked Date:" + DateFormat.yMd().format(_selectedDate!))),
+                    TextButton(
+                      onPressed: _presentDatePicker,
+                      child: const Text(
+                        "ChooseDate",
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              ElevatedButton(onPressed: _SubmitData, child: Text("Add Transaction"))
+            ],
+          ),
         ),
       ),
     );
   }
 
   _SubmitData() {
-
-
     if (_amountController.text.isEmpty || _titleController.text.isEmpty || double.parse(_amountController.text) <= 0 || _selectedDate == null) {
       return;
     }
