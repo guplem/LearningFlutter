1753339b371da671bae0e501f22a87b70b0f2b50
guplem <guplem@gmail.com>
2021-12-17 18:31:21 +0100

    Content depending on orientation

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -13,7 +13,7 @@ void main() {
   //  DeviceOrientation.portraitUp,
   //  DeviceOrientation.portraitDown,
   //]).then((_) {
-    runApp(new MyApp());
+  runApp(new MyApp());
   //});
 }
 
@@ -73,6 +73,8 @@ class _MyHomePageState extends State<MyHomePage> {
     return transactions.where((element) => element.date.isAfter(DateTime.now().subtract(Duration(days: 7)))).toList();
   }
 
+  bool _showChart = false;
+
   // To create a floating window with a widget
   void StartAddNewTransaction(BuildContext context) {
     showModalBottomSheet(
@@ -84,6 +86,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final bool isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+
     final appBar = AppBar(
       title: const Text("Simple Transactions App"),
       actions: [IconButton(onPressed: () => StartAddNewTransaction(context), icon: Icon(Icons.add))],
@@ -94,14 +98,30 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Container(
-            child: Chart(recentTransactions),
-            height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.3,
-          ),
-          Container(
-            child: TransacionList(transactions, _deleteTransaction),
-            height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.7,
-          ),
+          if (isLandscape)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Show Chart"),
+                Switch(
+                    value: _showChart,
+                    onChanged: (newVal) {
+                      setState(() {
+                        _showChart = newVal;
+                      });
+                    }),
+              ],
+            ),
+          if (_showChart || !isLandscape)
+            Container(
+              child: Chart(recentTransactions),
+              height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * (isLandscape ? 0.7 : 0.3),
+            ),
+          if (!_showChart || !isLandscape)
+            Container(
+              child: TransacionList(transactions, _deleteTransaction),
+              height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.7,
+            ),
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
