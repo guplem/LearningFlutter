8ede31a7d76996beaeb3de794e6277b5901755c8
guplem <guplem@gmail.com>
2021-12-21 18:02:50 +0100

    Details of meals

--- meals_app/lib/screens/meal_detail_screen.dart
+++ meals_app/lib/screens/meal_detail_screen.dart
@@ -12,7 +12,75 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Text("Meal detail screen: ${meal.id}"),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            BuildSectionTitle(context, "Ingredients"),
+            BuildSectionContainer(
+              context,
+              ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (ctx, index) => Card(
+                  color: Theme.of(context).colorScheme.secondary,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    child: Text(meal.ingredients[index]),
+                  ),
+                ),
+              ),
+            ),
+            BuildSectionTitle(context, "Steps"),
+            BuildSectionContainer(
+              context,
+              ListView.builder(
+                itemCount: meal.steps.length,
+                itemBuilder: (ctx, index) => Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text(
+                          "# ${index + 1}",
+                        ),
+                      ),
+                      title: Text(meal.steps[index]),
+                    ),
+                    if (index < meal.steps.length-1) Divider(),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget BuildSectionTitle(BuildContext ctx, String title) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(ctx).textTheme.headline6,
+      ),
+    );
+  }
+
+  Widget BuildSectionContainer(BuildContext ctx, Widget child) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.white, border: Border.all(color: Colors.grey), borderRadius: BorderRadius.circular(10)),
+      height: 150,
+      width: MediaQuery.of(ctx).size.width,
+      child: child,
+      margin: EdgeInsets.symmetric(horizontal: 15),
+      padding: EdgeInsets.symmetric(horizontal: 10),
     );
   }
 }
