8690dabbaac869084a13a9cbbf880175e00495fd
guplem <guplem@gmail.com>
2021-12-21 11:17:38 +0100

    Categories screen as main screen

--- meals_app/lib/categories_screen.dart
+++ meals_app/lib/categories_screen.dart
@@ -7,14 +7,16 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((catData) => CategoryItem(title: catData.title, color: catData.color)).toList(),
       ),
-      children: DUMMY_CATEGORIES.map((catData) => CategoryItem(title: catData.title, color: catData.color)).toList(),
     );
   }
 }
